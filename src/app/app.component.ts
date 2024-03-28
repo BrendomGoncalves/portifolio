0b3d58b78642b67d82829635b19025c4631d3f24
BrendomGoncalves <brendom.vi@gmail.com>
2024-03-28 00:34:20 -0300

    Atualizações de estilo e funcionalidade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Renderer2, ElementRef, AfterViewInit } from '@angular/core';import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -13,5 +12,18 @@ import { RouterModule } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'portifoliodev';
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.renderer.listen(this.el.nativeElement, 'click', (event) => {
+      if (event.target.matches('.nav-link')) {
+        const button = this.el.nativeElement.querySelector('.btn-close');
+        button.click();
+      }
+    });
+  }
+
 }
